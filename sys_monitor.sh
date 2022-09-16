a1a040dd6fb0f5ed48b7814bf68dbf78ed2f80b3
Jayaraju bura <92459601+jayaraju-bura@users.noreply.github.com>
2022-09-16 23:20:19 +0530

    Update sys_monitor.sh

--- sys_monitor.sh
+++ sys_monitor.sh
@@ -14,6 +14,11 @@ while [ "$iter" -lt "100" ]; do
 memory_used=$(free -m | awk 'NR==2{printf "%.2f%\t\t", $3*100/$2 }')
 disk_used=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}')
 cpu_used=$(top -bn1 | grep load | awk '{printf "   %.2f%%\t\t\n", $(NF-2)}')
+
+# we can use 'uptime' as well to get load average as an alternative 
+#>> uptime
+# 17:48:51 up 21 days, 11:07,  6 users,  load average: 0.00, 0.00, 0.12
+
 echo "$memory_used$disk_used$cpu_used"
 sleep 5
 done
