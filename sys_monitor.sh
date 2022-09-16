5c32ac782dd64933de765fd1673c47fbc602d469
Jayaraju bura <92459601+jayaraju-bura@users.noreply.github.com>
2022-09-16 23:17:53 +0530

    Create sys_monitor.sh

---
+++ sys_monitor.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+# This shell script monitors important system stats like memory, cpu and disk and prints them in tabulated format 
+
+
+printf "Memory \t\tDisk \t\t    CPU \n"
+printf "Usage  \t\tUsage \t\t Utilization\n"
+
+iter=0
+
+# use "while true" to keep the script alive in infinite loop
+
+while [ "$iter" -lt "100" ]; do
+memory_used=$(free -m | awk 'NR==2{printf "%.2f%\t\t", $3*100/$2 }')
+disk_used=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}')
+cpu_used=$(top -bn1 | grep load | awk '{printf "   %.2f%%\t\t\n", $(NF-2)}')
+echo "$memory_used$disk_used$cpu_used"
+sleep 5
+done
+
+# Sample output
+# Memory          Disk                CPU 
+# Usage           Usage            Utilization
+# 1.11%           82%                0.06%
+# 1.12%           82%                0.06%
+# 1.11%           82%                0.05%
